7884592de2c7083d80ba34e9999f6a30af884026
Renee Sarmiento <72420468+CptRainy@users.noreply.github.com>
2023-11-20 15:22:45 +0000

    Day 1

--- submissions/day-01-challenge.py
+++ submissions/day-01-challenge.py
@@ -1,1 +1,7 @@
+name = "Renee Sarmiento"
+department = "Department of Software and Web Development (DSWD)"
+team = "Frontend"
+description = "I am a workaholic caffeine addict that loves the satisfaction of pursuing different skills and knowledge"
 
+print(f"Hello, I am {name}. I love the {department} and I love being in the {team} even more! 
+      One thing about me is that {description}.")
